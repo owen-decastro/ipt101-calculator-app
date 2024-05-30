e7421caa23d890aca8eb64e272631044c3fb0327
DE CASTRO, MARK JOHN WELL D <162133191+owen-decastro@users.noreply.github.com>
2024-05-30 18:34:35 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,43 @@
+let currentInput='';
+
+function appendnumber(number){
+    currentInput += number;
+    document.getElementById('result').value =  currentInput;
+}
+function operators(operator) {
+    currentInput += operator;
+    document.getElementById('result').value = currentInput;
+}
+
+function calc(){
+    const result = eval(currentInput);
+    document.getElementById('result').value = result;
+    currentInput =  result.toString();
+}
+
+function clear() {
+    currentInput = '';
+    document.getElementById('result').value = '';
+}
+
+function del(){
+    value = document.getElementById("result").value;
+    val = document.getElementById("result").value = value.slice(0, - 1); 
+    currentInput = val;
+    
+}    
+
+function modulo(){
+    mult = eval(currentInput / 100);
+    total = document.getElementById('result').value = mult;
+    currentInput =  total;
+
+}
+
+function squareroot(){
+    value = document.getElementById('result').value
+    total = Math.sqrt(value)
+    showTotal = document.getElementById('result').value = total;
+    currentInput = showTotal
+}
+
